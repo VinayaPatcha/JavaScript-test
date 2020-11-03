1593c4e852af44f26d61675caa9a71d88a6805cb
VinayaPatcha <35723714+VinayaPatcha@users.noreply.github.com>
2020-11-03 23:41:16 +0530

    Add files via upload

--- javscript.js
+++ javscript.js
@@ -4,14 +4,14 @@ const api_url =
 
 
 
- 
+
 
 async function getapi(url) {
 
-  
+
     const response = await fetch(url);
 
-   
+
     var data = await response.json();
     console.log(data);
     if (response) {
@@ -19,39 +19,65 @@ async function getapi(url) {
     }
     show(data);
 }
-async function Deleteapi(url) {
+async function edt() {
+
+    var alt = $('input[type=checkbox]:checked').attr('id');
+
+    var splt = alt.split("_");
+
+    //     fetch('https://jsonplaceholder.typicode.com/users/1')
+    //   .then((response) => response.json())
+    //   .then((json) => console.log(json))
+    const res1 = await fetch('https://jsonplaceholder.typicode.com/users/' + splt[1]);
+
+
+    var data = await res1.json();
+    console.log(data["name"]);
+    $("#name").val(data["name"]);
+    $("#username").val(data["username"]);
+    $("#email").val(data["email"]);
+    $("#phone").val(data["phone"]);
+    $("#website").val(data["website"]);
+    $("#dialog-form1").dialog("open");
+
 }
 async function updateapi(url) {
 }
- 
+
 getapi(api_url);
- 
+
 
 function OnSelect(cnt) {
-
-
-
- 
     var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-
     if (checkboxes.length == 1) {
-        document.getElementById('dlt').removeAttribute('disabled', true);
+        document.getElementById('edt').removeAttribute('disabled', true);
     }
     else {
-        document.getElementById('dlt').setAttribute('disabled', true);
+        document.getElementById('edt').setAttribute('disabled', true);
     }
-
-
+}
+function dlt() {
+   
+    $.each($('input[type="checkbox"]:checked'), function(){
+    //    alert(splt[1]);
+    var alt =$(this).attr('id');
+    var splt = alt.split("_");
+    alert(splt[1]);
+    fetch('https://jsonplaceholder.typicode.com/users/'+splt[1], {
+              method: 'DELETE',
+         })
+    });
 }
 
 
- 
 function hideloader() {
     document.getElementById('loading').style.display = 'none';
 }
- 
+
 function show(data) {
-    document.getElementById('dlt').setAttribute('disabled', true);
+
+
+    document.getElementById('edt').setAttribute('disabled', true);
     var col = [];
     for (var i = 0; i < data.length; i++) {
         for (var key in data[i]) {
@@ -60,29 +86,21 @@ function show(data) {
             }
         }
     }
-
     console.log(col);
-    var table = document.createElement("table");
-
-    
-
-    var tr = table.insertRow(-1);                  
-
+    var table = document.createElement("table"); 
+    var tr = table.insertRow(-1);
     for (var i = 0; i < col.length; i++) {
         console.log('column', col[i]);
         if (col[i].toString() != 'address' && col[i].toString() != 'company') {
-            var th = document.createElement("th");     
+            var th = document.createElement("th");
             th.innerHTML = col[i];
             tr.appendChild(th);
 
         }
-
     }
-    var th = document.createElement("th");     
+    var th = document.createElement("th");
     th.innerHTML = 'Select';
     tr.appendChild(th);
-
-   
     for (var i = 0; i < data.length; i++) {
 
         tr = table.insertRow(-1);
@@ -93,26 +111,13 @@ function show(data) {
 
             if (col[j].toString() != 'address' && col[j].toString() != 'company') {
                 var tabCell = tr.insertCell(-1);
-
-
                 tabCell.innerHTML = data[i][col[j]];
-
-           
-
             }
             td.innerHTML = `<input type="checkbox" name="chkrows" onchange='OnSelect(this);' id=chk_` + data[i][col[0]] + `>`;
             tr.appendChild(td);
-
         }
-
-
     }
-
-   
     var divContainer = document.getElementById("showData");
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
-
-
-
 } 
--- modal.js
+++ modal.js
@@ -1,81 +1,130 @@
-$(function() {
-    
-    $( "#dialog:ui-dialog" ).dialog( "destroy" );
+$(function () {
 
-    var name = $( "#name" ),
-        email = $( "#email" ),
-        password = $( "#password" ),
-        allFields = $( [] ).add( name ).add( email ).add( password ),
-        tips = $( ".validateTips" );
+    $("#dialog:ui-dialog").dialog("destroy");
 
-    function updateTips( t ) {
+    var name = $("#name"),
+        email = $("#email"),
+        password = $("#password"),
+        allFields = $([]).add(name).add(email).add(password),
+        tips = $(".validateTips");
+
+    function updateTips(t) {
         tips
-        .text( t )
-        .addClass( "ui-state-highlight" );
-        setTimeout(function() {
-            tips.removeClass( "ui-state-highlight", 1500 );
-        }, 500 );
+            .text(t)
+            .addClass("ui-state-highlight");
+        setTimeout(function () {
+            tips.removeClass("ui-state-highlight", 1500);
+        }, 500);
     }
 
-    function checkLength( o, n, min, max ) {
-        if ( o.val().length > max || o.val().length < min ) {
-            o.addClass( "ui-state-error" );
-            updateTips( "Length of " + n + " must be between " +
-                    min + " and " + max + "." );
+    function checkLength(o, n, min, max) {
+        if (o.val().length > max || o.val().length < min) {
+            o.addClass("ui-state-error");
+            updateTips("Length of " + n + " must be between " +
+                min + " and " + max + ".");
             return false;
         } else {
             return true;
         }
     }
 
-    function checkRegexp( o, regexp, n ) {
-        if ( !( regexp.test( o.val() ) ) ) {
-            o.addClass( "ui-state-error" );
-            updateTips( n );
+    function checkRegexp(o, regexp, n) {
+        if (!(regexp.test(o.val()))) {
+            o.addClass("ui-state-error");
+            updateTips(n);
             return false;
         } else {
             return true;
         }
     }
 
-    $( "#dialog-form" ).dialog({
+    $("#dialog-form").dialog({
         autoOpen: false,
         height: 600,
         width: 650,
         modal: true,
         buttons: {
-            "Create new User": function() {
+            "Create new User": function () {
+                $(this).attr('id', 'nwUsr');
                 var bValid = true;
-                allFields.removeClass( "ui-state-error" );
+                allFields.removeClass("ui-state-error");
 
-                bValid = bValid && checkLength( name, "username", 3, 16 );
-                bValid = bValid && checkLength( email, "email", 6, 80 );
-                bValid = bValid && checkLength( password, "password", 5, 16 );
-
-                bValid = bValid && checkRegexp( name, /^[a-z]([0-9a-z_])+$/i, "Username may consist of a-z, 0-9, underscores, begin with a letter." );
-                
-                bValid = bValid && checkRegexp( email, /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i, "eg. ui@jquery.com" );
-                bValid = bValid && checkRegexp( password, /^([0-9a-zA-Z])+$/, "Password field only allow : a-z 0-9" );
-
-                if ( bValid ) {
-                    $( "#users tbody" ).append( "<tr>" +
-                        "<td>" + name.val() + "</td>" + 
-                        "<td>" + email.val() + "</td>" + 
-                        "<td>" + password.val() + "</td>" +
-                        "</tr>" ); 
-                    $( this ).dialog( "close" );
+              
+                if (bValid) {
+                    fetch('https://jsonplaceholder.typicode.com/posts', {
+                        method: 'POST',
+                        body: JSON.stringify({
+                            name: $('name1').val(),
+                            username: $('username1').val(),
+                            email: $('email1').val(),
+                            phone: $('phone1').val(),
+                            website: $('website1').val()
+                        }),
+                        headers: {
+                          'Content-type': 'application/json; charset=UTF-8',
+                        },
+                      })
+                        .then((response) => response.json())
+                        .then((json) => console.log(json))
                 }
             },
-                Cancel: function() {
-                    $( this ).dialog( "close" );
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        },
+        close: function () {
+            allFields.val("").removeClass("ui-state-error");
+        }
+    });
+    $("#dialog-form1").dialog({
+        autoOpen: false,
+        height: 600,
+        width: 650,
+        modal: true,
+        buttons: {
+            "Update User": function () {
+                $(this).attr('id', 'nwUsr');
+                var bValid = true;
+                allFields.removeClass("ui-state-error");
+ 
+
+                if (bValid) {
+                 
+                    var alt = $('input[type=checkbox]:checked').attr('id');
+
+                    var splt = alt.split("_");
+                    fetch('https://jsonplaceholder.typicode.com/users/' + splt[1], {
+                        method: 'PATCH',
+                        body: JSON.stringify({
+                            id: splt[1],
+                            name: $('name').val(),
+                            username: $('username').val(),
+                            email: $('email').val(),
+                            phone: $('phone').val(),
+                            website: $('website').val()
+                        }),
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8',
+                        },
+                    })
+                        .then((response) => response.json())
+                        .then((json) => console.log(json))
+                   
+
+
+                    $(this).dialog("close");
                 }
             },
-        close: function() {
-            allFields.val( "" ).removeClass( "ui-state-error" );
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        },
+        close: function () {
+            allFields.val("").removeClass("ui-state-error");
         }
     });
 
-    $( "#create-user" ).button().click(function() {
-        $( "#dialog-form" ).dialog( "open" );
+    $("#create-user").button().click(function () {
+        $("#dialog-form").dialog("open");
     });
 });
